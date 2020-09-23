665c80a45148815eafd28f763a7cc563f94c4259
amandacutrer <23acutrer@athenian.org>
2020-09-23 23:14:46 +0000

    got the caesar methods to work

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,55 +2,91 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-    char ch;
     String newMessage = "";
-    for (int i = 0; i < message.length(); i++){ 
-        if (message.charAt(i) < 'A' || message.charAt(i) > 'Z'){
-            newMessage = newMessage + message.charAt(i);
-            continue;
+        for (int i = 0; i < message.length(); i++){
+        char currentChar = message.charAt(i);
+        if (currentChar >= 'A' && currentChar <= 'Z'){
+                newMessage = newMessage + (char)((currentChar - 62)%26+65);
         }
-        char newChar = (char)(message.charAt(i) + 3);
-        if (newChar > 'Z'){
-            ch = (char)((newChar) - 26);
-        }
-        else {
-            ch = (char)(newChar);
-        }
-        newMessage = newMessage + ch;
+        else if (currentChar >= 'a' && currentChar <= 'z'){
+                newMessage = newMessage + (char)((currentChar - 94)%26+97);
+            }
+         else{
+            newMessage = newMessage + currentChar;
+         }
+        
     }
         return newMessage;
-
     }
 
     public static String decryptCaesar(String message) {
-        char ch;
-    String newMessage = "";
-    for (int i = 0; i < message.length(); i++){
-        if (message.charAt(i) < 'A' || message.charAt(i) > 'Z'){
-            newMessage = newMessage + message.charAt(i);
-            continue;
+     String newMessage = "";
+        for (int i = 0; i < message.length(); i++){
+        char currentChar = message.charAt(i);
+        if (currentChar >= 'D' && currentChar <= 'Z'){
+                newMessage = newMessage + (char)(currentChar - 3);
         }
-        else if (message.charAt(i) > 'A' && message.charAt(i) < 'Z'){
-            if (message.charAt(i - 3) < 'A'){
-            ch = (char)(message.charAt(i) + 23);
+        else if (currentChar >= 'd' && currentChar <= 'z'){
+                newMessage = newMessage + (char)(currentChar - 3);
+            }
+        else if (currentChar >= 'a' && currentChar <= 'c'){
+            newMessage = newMessage + (char)((currentChar + - 74)%26+97);
         }
-        else {
-            ch = (char)(message.charAt(i) - 3);
+        else if (currentChar >= 'A' && currentChar <= 'C'){
+            newMessage = newMessage + (char)((currentChar + - 42)%26+65);
         }
-        newMessage = newMessage + ch;
+         else{
+            newMessage = newMessage + currentChar;
+         }
+        
+    }
+        return newMessage;
+    }
+    
+
+    public static String encryptCaesarKey(String message, int key) {
+        String newMessage = "";
+        for (int i = 0; i < message.length(); i++){
+        char currentChar = message.charAt(i);
+        if (currentChar >= 'A' && currentChar <= 'Z'){
+                newMessage = newMessage + (char)((currentChar - 65 + key)%26+65);
         }
+        else if (currentChar >= 'a' && currentChar <= 'z'){
+                newMessage = newMessage + (char)((currentChar - 97 + key)%26+97);
+            }
+         else{
+            newMessage = newMessage + currentChar;
+         }
+        
     }
         return newMessage;
     }
 
-    public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
-    }
-
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String newMessage = "";
+        if (key > 26){
+            key = key%26;
+        }
+        for (int i = 0; i < message.length(); i++){
+        char currentChar = message.charAt(i);
+        if (currentChar >= ('A'+key) && currentChar <= 'Z'){
+                newMessage = newMessage + (char)(currentChar - key);
+        }
+        else if (currentChar >= ('a'+key) && currentChar <= 'z'){
+                newMessage = newMessage + (char)(currentChar - key);
+            }
+        else if (currentChar >= 'a' && currentChar <= ('a'+ key)){
+            newMessage = newMessage + (char)((currentChar - 71 - key)%26+97);
+        }
+        else if (currentChar >= 'A' && currentChar <= ('A'+ key)){
+            newMessage = newMessage + (char)((currentChar - 39 - key)%26+65);
+        }
+         else{
+            newMessage = newMessage + currentChar;
+         }
+        
+    }
+        return newMessage;
     }
 
 
@@ -63,14 +99,18 @@ public class Caesar {
         if (command.equals("encrypt")) {
             System.out.println("Please enter your message to be encrypted: ");
             String message = scan.nextLine();
+            System.out.println("Please enter the key for your message: ");
+            int key = scan.nextInt();
             System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesar(message));
+            System.out.println(encryptCaesarKey(message, key));
         }
         else if (command.equals("decrypt")) {
             System.out.println("Please enter your message to be decrypted: ");
             String message = scan.nextLine();
+            System.out.println("Please enter the key for your message: ");
+            int key = scan.nextInt();
             System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesar(message));
+            System.out.println(decryptCaesarKey(message, key));
         }
         else {
             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
